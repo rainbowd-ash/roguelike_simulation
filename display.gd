ac8ca8e6c230763f5d63e3f36a2727907da1d389
ash <ash@ashes.computer>
2026-04-17 20:16:58 -0700

    convert terrain tiles to a bunch of sprites instead of a tilemap

--- display.gd
+++ display.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+@onready var tile_sprite: Sprite2D = $Tile
+
+var tile_size = 10
+
 func _ready() -> void:
+	tile_sprite.hide()
 	for child in %Entities.get_children():
 		child.changed_position.connect(on_entity_changed_position)
 	draw_everything()
@@ -11,12 +16,24 @@ func draw_everything():
 	draw_entities()
 
 func draw_terrain():
-	%TerrainTileMap.clear()
 	var terrain = %World.terrain
 	for y in terrain.size():
 		for x in terrain[y].size():
-			var sprite_id = terrain[y][x]
-			%TerrainTileMap.set_cell(Vector2(x, y),0,terrain[y][x].sprite_id)
+			var sprite_id = terrain[y][x].sprite_id
+			var new_tile = tile_sprite.duplicate()
+			$TerrainTiles.add_child(new_tile)
+			new_tile.show()
+			new_tile.position = Vector2(
+				x * tile_size + tile_size/2, 
+				y * tile_size + tile_size/2)
+			new_tile.region_rect = Rect2(sprite_id.x * tile_size, sprite_id.y * tile_size, tile_size, tile_size)
+	
+	#%TerrainTileMap.clear()
+	#var terrain = %World.terrain
+	#for y in terrain.size():
+		#for x in terrain[y].size():
+			#var sprite_id = terrain[y][x]
+			#%TerrainTileMap.set_cell(Vector2(x, y),0,terrain[y][x].sprite_id)
 
 func draw_recharge_machines():
 	%MachinesTileMap.clear()
