ae4119ffa62ffb6543e34815fe698a5a7789589d
ash <ash@ashes.computer>
2026-04-19 14:47:16 -0700

    the bot can nav around again

--- robot_components/entity_base.gd
+++ robot_components/entity_base.gd
@@ -1,10 +1,20 @@
 class_name Entity
-extends Node
+extends Node2D
 
 @export var power_source : PowerSource
 
-var world_position : Vector2i = Vector2i.ZERO
+var grid_position : Vector2i
 
-func move_to(position : Vector2i):
-	var prev_position = world_position
-	world_position = position
+var movement_path : Array
+var destination_tile = Vector2i(10,20)
+
+func _ready() -> void:
+	grid_position = Grid.position_to_grid(position)
+
+func _physics_process(_delta: float) -> void:
+	if grid_position == destination_tile:
+		return
+	if movement_path.size() == 0:
+		movement_path = %AStarGrid.astar_grid.get_id_path(grid_position,destination_tile)
+	var next_tile = movement_path.pop_front()
+	position = Grid.grid_to_position(next_tile)
--- robot_components/navigation.gd
+++ robot_components/navigation.gd
@@ -1,5 +1,18 @@
 class_name Navigation
 extends Node
 
+var grid_position : Vector2i
+
 var movement_path : Array
 var destination_tile = Vector2i(10,20)
+
+func _ready() -> void:
+	grid_position = Grid.position_to_grid(get_parent().position)
+
+func _physics_process(_delta: float) -> void:
+	if grid_position == destination_tile:
+		return
+	if movement_path.size() == 0:
+		movement_path = %AStarGrid.astar_grid.get_id_path(grid_position,destination_tile)
+	var next_tile = movement_path.pop_front()
+	get_parent().position = Grid.grid_to_position(grid_position)
