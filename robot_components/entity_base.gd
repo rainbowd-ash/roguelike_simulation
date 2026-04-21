1589bbcbe0af0da18b8197d4b6e835b80f04041e
ash <ash@ashes.computer>
2026-04-20 23:11:43 -0700

    A I

--- robot_components/entity_base.gd
+++ robot_components/entity_base.gd
@@ -2,11 +2,11 @@ class_name Entity
 extends Node2D
 
 var grid_position : Vector2i
+@onready var world: World = $"../.."
 
 var movement_path : Array
-var destination_tile = Vector2i(10,20)
 
-var movespeed : int = 30
+var movespeed : int = 5
 var wait : int = 0
 
 func _ready() -> void:
@@ -14,18 +14,15 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if wait == 0:
-		move_along_path()
+		ai()
 	else:
 		wait = wait - 1
 
-func move_along_path():
-	if grid_position == destination_tile:
-		return
+func ai():
 	if movement_path.size() == 0:
-		movement_path = %AStarGrid.astar_grid.get_id_path(grid_position,destination_tile)
-	var next_tile = movement_path.pop_front()
-	position = Grid.grid_to_position(next_tile)
-	wait = movespeed
-
-func find_nearest_recharge(grid_position, %RechargeMachines.machine_locations):
-	
+		var new_destination = Vector2i(randi_range(0,world.width),randi_range(0,world.height))
+		movement_path = %AStarGrid.astar_grid.get_id_path(grid_position,new_destination)
+	else:
+		grid_position = movement_path.pop_front()
+		position = Grid.grid_to_position(grid_position)
+		wait = movespeed
